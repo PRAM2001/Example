8b9bad7ca24592516a7a3c6724323e919f9619aa
PRAM2001 <jadhavpramod1432@gmail.comgit config --global user.name PRAM2001git commitgit config --global user.email jadhavpramod1432@gmail.com>
2023-12-14 13:51:34 +0530

    some messege

---
+++ src/com/user.java
@@ -0,0 +1,22 @@
+package com;
+
+public class user {
+	private int userId;
+	private String userName;
+	private String userEMail;
+	private String userPassword;
+	public int getUserId() {
+	return userId; 
+}
+	public void setterEmail(String userEmail) {
+		this.userEMail=userEmail;	
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	
+	public void setUserPassword(String userPassword) {
+		this.userPassword=userPassword;
+	}
+}
+
